764ad08b65e43729d0e4fa5d2586adedbb73426a
kchranger <kchranger@gmail.com>
2024-10-09 21:35:14 +0900

    chore: 초기 환경 설정 #1
    - java 버전 21 설정
    - 의존성 설정

---
+++ todaktodak-api/src/main/java/com/heartsave/todaktodak_api/TodaktodakApiApplication.java
@@ -0,0 +1,14 @@
+package com.heartsave.todaktodak_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class TodaktodakApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaktodakApiApplication.class, args);
+	}
+
+}
---
+++ todaktodak-api/src/test/java/com/heartsave/todaktodak_api/TodaktodakApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartsave.todaktodak_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaktodakApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
