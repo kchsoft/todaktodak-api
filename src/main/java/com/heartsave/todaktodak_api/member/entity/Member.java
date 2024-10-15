3a7621238818a69592e985a33ef62aaebdd579e2
Sin Han <vrdhan212@gmail.com>
2024-10-15 16:03:45 +0900

    feat: 회원 엔티티 TODAK-61

---
+++ src/main/java/com/heartsave/todaktodak_api/member/entity/Member.java
@@ -0,0 +1,33 @@
+package com.heartsave.todaktodak_api.member.entity;
+
+import com.heartsave.todaktodak_api.common.type.AuthType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50)
+    private String email;
+
+    @Column(length = 50)
+    private String nickname;
+
+    @Column(length = 30)
+    private String loginId;
+
+    @Column(length = 30)
+    private String password;
+
+    private String characterImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private AuthType authType;
+}
