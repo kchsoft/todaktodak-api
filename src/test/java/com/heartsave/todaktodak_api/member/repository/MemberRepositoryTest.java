1ec600702141fcbae3ec8a1901f61d4526ce504f
Sin Han <vrdhan212@gmail.com>
2024-10-15 17:47:29 +0900

    test: 회원 레포지토리 필드 중복 테스트 TODAK-61

--- src/test/java/com/heartsave/todaktodak_api/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/heartsave/todaktodak_api/member/repository/MemberRepositoryTest.java
@@ -6,23 +6,43 @@ import com.heartsave.todaktodak_api.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryTest {
   @Autowired private MemberRepository memberRepository;
 
   @Test
-  @DisplayName("연동 확인을 위한 회원 저장")
-  void testSaveMember() {
+  @DisplayName("로그인 아이디로 회원 조회")
+  void getMemberByLoginIdTest() {
 
     // Given
-    Member member = Member.builder().email("test@test.com").build();
+    String loginId = "TEST_ID1";
+    Member member = Member.builder().loginId(loginId).build();
+    memberRepository.save(member);
 
     // When
-    Member retrievedMember = memberRepository.save(member);
+    var retrievedMember = memberRepository.findMemberByLoginId(loginId);
 
     // Then
-    assertThat(retrievedMember.getEmail()).isEqualTo(member.getEmail());
+    assertThat(retrievedMember.isPresent()).isEqualTo(true);
+  }
+
+  @Test
+  @DisplayName("닉네임으로 회원 조회")
+  void getMemberByNicknameTest() {
+
+    // Given
+    String nickname = "TEST_NICKNAME";
+    Member member = Member.builder().nickname(nickname).build();
+    memberRepository.save(member);
+
+    // When
+    var retrievedMember = memberRepository.findMemberByNickname(nickname);
+
+    // Then
+    assertThat(retrievedMember.isPresent()).isEqualTo(true);
   }
 }
