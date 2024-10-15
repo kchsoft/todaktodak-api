3d7d6b5854fffdb8c47163e347d2419a63c6646a
Sin Han <vrdhan212@gmail.com>
2024-10-15 17:32:47 +0900

    feat: 회원 정보 중복확인 DB 메서드
    로그인 아이디와 닉네임 TODAK-61

--- src/main/java/com/heartsave/todaktodak_api/member/repository/MemberRepository.java
+++ src/main/java/com/heartsave/todaktodak_api/member/repository/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.heartsave.todaktodak_api.member.repository;
 
 import com.heartsave.todaktodak_api.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  Optional<Member> findMemberByLoginId(String loginId);
+
+  Optional<Member> findMemberByNickname(String nickname);
 }
