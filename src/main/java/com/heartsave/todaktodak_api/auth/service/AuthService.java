3448474a3cd3b9af1bcfae3ba51affab7209a3f4
Sin Han <vrdhan212@gmail.com>
2024-10-15 21:07:03 +0900

    rename: 인증 서비스에서 중복 기능 담당 TODAK-61

--- src/main/java/com/heartsave/todaktodak_api/auth/service/AuthService.java
+++ src/main/java/com/heartsave/todaktodak_api/auth/service/AuthService.java
@@ -1,27 +1,21 @@
-package com.heartsave.todaktodak_api.member.service;
+package com.heartsave.todaktodak_api.auth.service;
 
-import com.heartsave.todaktodak_api.member.dto.LoginIdCheckReq;
-import com.heartsave.todaktodak_api.member.dto.NicknameCheckReq;
+import com.heartsave.todaktodak_api.auth.dto.LoginIdCheckReq;
+import com.heartsave.todaktodak_api.auth.dto.NicknameCheckReq;
 import com.heartsave.todaktodak_api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-@Primary
 @Service
-public class MemberServiceImpl implements MemberService {
+@RequiredArgsConstructor
+public class AuthService {
   private final MemberRepository memberRepository;
 
-  @Override
   public boolean isDuplicatedLoginId(LoginIdCheckReq dto) {
     String targetId = dto.loginId();
     return memberRepository.findMemberByLoginId(targetId).isPresent();
   }
 
-  @Override
   public boolean isDuplicatedNickname(NicknameCheckReq dto) {
     String targetNickname = dto.nickname();
     return memberRepository.findMemberByNickname(targetNickname).isPresent();
--- src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
+++ src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
@@ -1,10 +1,3 @@
 package com.heartsave.todaktodak_api.member.service;
 
-import com.heartsave.todaktodak_api.member.dto.LoginIdCheckReq;
-import com.heartsave.todaktodak_api.member.dto.NicknameCheckReq;
-
-public interface MemberService {
-  boolean isDuplicatedNickname(NicknameCheckReq dto);
-
-  boolean isDuplicatedLoginId(LoginIdCheckReq dto);
-}
+public interface MemberService {}
