ff96c2bbc531a30f90a1153c0160c359ef9f7677
Sin Han <vrdhan212@gmail.com>
2024-10-15 16:03:16 +0900

    feat: 회원가입 유형 enum TODAK-61

---
+++ src/main/java/com/heartsave/todaktodak_api/common/type/AuthType.java
@@ -0,0 +1,17 @@
+package com.heartsave.todaktodak_api.common.type;
+
+import lombok.Getter;
+
+@Getter
+public enum AuthType {
+    BASE("BASE"),
+    KAKAO("KAKAO"),
+    NAVER("NAVER"),
+    GOOGLE("GOOGLE");
+
+    private final String type;
+
+    AuthType(String type) {
+        this.type = type;
+    }
+}
