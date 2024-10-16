51f4648c009d5b3e109691265b671c666db4cdfe
Sin Han <vrdhan212@gmail.com>
2024-10-16 18:01:05 +0900

    refactor: 회원정보 중복 확인 API 명세화 TODAK-92

--- src/main/java/com/heartsave/todaktodak_api/auth/controller/AuthController.java
+++ src/main/java/com/heartsave/todaktodak_api/auth/controller/AuthController.java
@@ -3,6 +3,10 @@ package com.heartsave.todaktodak_api.auth.controller;
 import com.heartsave.todaktodak_api.auth.dto.LoginIdCheckRequest;
 import com.heartsave.todaktodak_api.auth.dto.NicknameCheckRequest;
 import com.heartsave.todaktodak_api.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,18 +16,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "인증", description = "인증 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
   private final AuthService authService;
 
+  @Operation(summary = "닉네임 중복 확인")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "204", description = "유일한 닉네임"),
+        @ApiResponse(responseCode = "409", description = "중복 닉네임 존재")
+      })
   @PostMapping("/nickname")
   ResponseEntity<Void> checkNickname(@Valid @RequestBody NicknameCheckRequest dto) {
     if (!authService.isDuplicatedNickname(dto)) return ResponseEntity.noContent().build();
     return ResponseEntity.status(HttpStatus.CONFLICT).build();
   }
 
+  @Operation(summary = "로그인 ID 중복 확인")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "204", description = "유일한 아이디"),
+        @ApiResponse(responseCode = "409", description = "중복 아이디 존재")
+      })
   @PostMapping("/login-id")
   ResponseEntity<Void> checkLoginId(@Valid @RequestBody LoginIdCheckRequest dto) {
     if (!authService.isDuplicatedLoginId(dto)) return ResponseEntity.noContent().build();
--- src/main/java/com/heartsave/todaktodak_api/auth/dto/NicknameCheckRequest.java
+++ src/main/java/com/heartsave/todaktodak_api/auth/dto/NicknameCheckRequest.java
@@ -1,7 +1,12 @@
 package com.heartsave.todaktodak_api.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "닉네임 중복 확인 요청 데이터")
 public record NicknameCheckRequest(
-    @NotBlank(message = "Blank data") @Size(max = 50, message = "Exceed size") String nickname) {}
+    @Schema(example = "todak", description = "최대 50자")
+        @NotBlank(message = "Blank data")
+        @Size(max = 50, message = "Exceed size")
+        String nickname) {}
