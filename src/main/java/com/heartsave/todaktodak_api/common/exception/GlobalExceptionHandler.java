2cf7ae588930c725267360a206950e67ccb755c5
Sin Han <vrdhan212@gmail.com>
2024-10-16 17:18:08 +0900

    refactor: 유효성 검사 예외 메시지 파싱 TODAK-92

--- src/main/java/com/heartsave/todaktodak_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.heartsave.todaktodak_api.common.exception;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,14 +19,17 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_GATEWAY)
-  public ResponseEntity<List<String>> handleInvalidException(MethodArgumentNotValidException e) {
+  public ResponseEntity<List<Map<String, String>>> handleInvalidException(
+      MethodArgumentNotValidException e) {
     var errors = parseError(e);
     return ResponseEntity.badRequest().body(errors);
   }
 
-  private List<String> parseError(BindException e) {
+  private List<Map<String, String>> parseError(BindException e) {
+    logger.info(String.valueOf(e.getBindingResult()));
+    logger.info(String.valueOf(e.getBindingResult().getFieldErrors()));
     return e.getBindingResult().getFieldErrors().stream()
-        .map(FieldError::getDefaultMessage)
+        .map((fe) -> Map.of(fe.getField(), Optional.ofNullable(fe.getDefaultMessage()).orElse("")))
         .toList();
   }
 }
