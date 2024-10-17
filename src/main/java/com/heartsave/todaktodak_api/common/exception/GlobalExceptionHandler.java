b5aa60b79808ac81542c6063fee862b2abd81e0e
Sin Han <vrdhan212@gmail.com>
2024-10-17 13:52:44 +0900

    refactor: 유효성 검사 실패에 대한 에러 응답 TODAK-92

--- src/main/java/com/heartsave/todaktodak_api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.heartsave.todaktodak_api.common.exception;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -17,19 +17,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
   private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+  // 유효성 검사 실패
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.BAD_GATEWAY)
-  public ResponseEntity<List<Map<String, String>>> handleInvalidException(
-      MethodArgumentNotValidException e) {
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ResponseEntity<ErrorResponse> handleInvalidExceptions(MethodArgumentNotValidException e) {
     var errors = parseError(e);
-    return ResponseEntity.badRequest().body(errors);
+    logger.info("PARSED ERROR: {}", errors);
+    return ResponseEntity.badRequest().body(ErrorResponse.from(errors));
   }
 
-  private List<Map<String, String>> parseError(BindException e) {
-    logger.info(String.valueOf(e.getBindingResult()));
-    logger.info(String.valueOf(e.getBindingResult().getFieldErrors()));
-    return e.getBindingResult().getFieldErrors().stream()
-        .map((fe) -> Map.of(fe.getField(), Optional.ofNullable(fe.getDefaultMessage()).orElse("")))
-        .toList();
+  private Map<String, String> parseError(BindException e) {
+    Map<String, String> fieldErrors = new HashMap<>();
+    e.getBindingResult()
+        .getFieldErrors()
+        .forEach(
+            (fe) ->
+                fieldErrors.put(
+                    fe.getField(), Optional.ofNullable(fe.getDefaultMessage()).orElse("")));
+    return fieldErrors;
   }
 }
