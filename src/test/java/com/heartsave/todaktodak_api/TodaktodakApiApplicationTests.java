d67d5d6ce5efd682b7a4f9d2fb8049df9ccf3455
kchranger <kchranger@gmail.com>
2024-10-17 13:16:09 +0900

    test: springboot 테스트 환경 설정 TODAK-79
    - springboot test에 사용할 test yaml 파일 활성화 적용
    - springboot test 설정 추상화 적용

--- src/test/java/com/heartsave/todaktodak_api/TodaktodakApiApplicationTests.java
+++ src/test/java/com/heartsave/todaktodak_api/TodaktodakApiApplicationTests.java
@@ -1,13 +1,10 @@
 package com.heartsave.todaktodak_api;
 
+import com.heartsave.todaktodak_api.config.BaseSpringbootTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TodaktodakApiApplicationTests {
+class TodaktodakApiApplicationTests extends BaseSpringbootTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
