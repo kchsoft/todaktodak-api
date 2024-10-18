f662cf73a635a91fae11d8aaf6aa1d24f5ee2d12
kchranger <kchranger@gmail.com>
2024-10-18 15:00:30 +0900

    feat: 일기 작성 API Swagger 적용 TODAK-79

--- src/main/java/com/heartsave/todaktodak_api/diary/controller/DiaryController.java
+++ src/main/java/com/heartsave/todaktodak_api/diary/controller/DiaryController.java
@@ -2,6 +2,10 @@ package com.heartsave.todaktodak_api.diary.controller;
 
 import com.heartsave.todaktodak_api.diary.dto.request.DiaryWriteRequest;
 import com.heartsave.todaktodak_api.diary.service.DiaryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +18,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "일기장", description = "일기장 API")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/diary")
+@RequestMapping("/api/v1/diary")
 public class DiaryController {
 
   private final DiaryService diaryService;
 
+  @Operation(summary = "일기 작성")
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "201", description = "일기 작성 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청")
+      })
   @PostMapping("/my")
   public ResponseEntity<Void> writeDiary(
       @AuthenticationPrincipal OAuth2User auth,
--- src/main/java/com/heartsave/todaktodak_api/diary/dto/request/DiaryWriteRequest.java
+++ src/main/java/com/heartsave/todaktodak_api/diary/dto/request/DiaryWriteRequest.java
@@ -5,23 +5,34 @@ import static com.heartsave.todaktodak_api.diary.common.DiaryContentConstraintCo
 import static com.heartsave.todaktodak_api.diary.common.DiaryContentConstraintConstant.DIARY_PUBLIC_CONTENT_MAX_SIZE;
 
 import com.heartsave.todaktodak_api.diary.common.DiaryEmotion;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 @Getter
+@Schema(description = "일기 작성 요청 데이터")
 public class DiaryWriteRequest {
-  @PastOrPresent private LocalDateTime date;
 
-  @NotNull private DiaryEmotion emotion;
+  @PastOrPresent(message = "Diary Writing Date is Future")
+  private LocalDateTime date;
 
-  @Size(min = DIARY_CONTENT_MIN_SIZE, max = DIARY_CONTENT_MAX_SIZE) // 임시 범위
+  @NotNull(message = "DiaryEmotion is Null")
+  private DiaryEmotion emotion;
+
+  @Range(
+      min = DIARY_CONTENT_MIN_SIZE,
+      max = DIARY_CONTENT_MAX_SIZE,
+      message = "Diary Content Length Out Of Range") // 임시 범위
   private String content;
 
-  @Size(max = DIARY_PUBLIC_CONTENT_MAX_SIZE) // 임시 범위
+  @Range(
+      max = DIARY_PUBLIC_CONTENT_MAX_SIZE,
+      message = "Diary Public Content Length Out Of Range") // 임시 범위
   private String publicContent;
 
-  @NotNull private Boolean isPublic;
+  @NotNull(message = "isPublic is Null")
+  private Boolean isPublic;
 }
