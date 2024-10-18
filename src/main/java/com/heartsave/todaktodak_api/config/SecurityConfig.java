36404ec159da138ae8097c73dd083f6b83d39672
Sin Han <vrdhan212@gmail.com>
2024-10-18 21:34:38 +0900

    feat: cors 설정 및 기본 스프링 시큐리티

---
+++ src/main/java/com/heartsave/todaktodak_api/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.heartsave.todaktodak_api.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .exceptionHandling(AbstractHttpConfigurer::disable)
+        .oauth2Login(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            authorize -> authorize.requestMatchers("").permitAll().anyRequest().authenticated())
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("**/", corsConfiguration());
+    return source;
+  }
+
+  private CorsConfiguration corsConfiguration() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedMethods(List.of("HEAD", "OPTION", "GET", "POST", "PATCH", "DELETE"));
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedOrigin("http://localhost:3000");
+    return configuration;
+  }
+}
