8972cf3ead356c3ef7b11d372b00d2f5ba07d570
Sin Han <vrdhan212@gmail.com>
2024-10-18 22:38:45 +0900

    refactor: 에러 명세 추가

--- src/main/java/com/heartsave/todaktodak_api/common/type/ErrorSpec.java
+++ src/main/java/com/heartsave/todaktodak_api/common/type/ErrorSpec.java
@@ -1,12 +1,15 @@
 package com.heartsave.todaktodak_api.common.type;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum ErrorSpec {
-  ;
+  NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않습니다.");
 
-  private HttpStatus status;
-  private String message;
+  private final HttpStatus status;
+  private final String message;
 }
--- src/main/java/com/heartsave/todaktodak_api/member/domain/MemberRole.java
+++ src/main/java/com/heartsave/todaktodak_api/member/domain/MemberRole.java
@@ -3,8 +3,8 @@ package com.heartsave.todaktodak_api.member.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum MemberRole implements GrantedAuthority {
-  TEMP,
-  USER;
+  ROLE_TEMP,
+  ROLE_USER;
 
   @Override
   public String getAuthority() {
