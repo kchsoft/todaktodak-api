e9f7e520787ce60087037ce563b0c686594ab1c9
kchranger <kchranger@gmail.com>
2024-10-20 21:11:22 +0900

    test: ai 컨텐츠 요청 테스트 수정 TODAK-79
    - ai 코멘트 응답 확인
    - 테스트 메서드 정의 변경(비동기 체크 -> ai 요청 체크)

--- src/test/java/com/heartsave/todaktodak_api/ai/service/AiServiceTest.java
+++ src/test/java/com/heartsave/todaktodak_api/ai/service/AiServiceTest.java
@@ -2,12 +2,12 @@ package com.heartsave.todaktodak_api.ai.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.heartsave.todaktodak_api.ai.dto.AiContentResponse;
 import com.heartsave.todaktodak_api.diary.constant.DiaryEmotion;
 import com.heartsave.todaktodak_api.diary.entity.DiaryEntity;
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
@@ -24,74 +24,65 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 class AiServiceTest {
 
-  private static MockWebServer mockWebServer;
-  private static AiService aiService;
-  private static final String WEBTOON_URI = "/webtoon";
-  private static final String BGM_URI = "/bgm";
-  private static final String COMMENT_URI = "/comment";
+    private static MockWebServer mockWebServer;
+    private static AiService aiService;
+    private static final String WEBTOON_URI = "/webtoon";
+    private static final String BGM_URI = "/bgm";
+    private static final String COMMENT_URI = "/comment";
+    private static final String AI_COMMENT = "this is ai comment";
 
-  @BeforeAll
-  static void setUp() throws IOException {
-    mockWebServer = new MockWebServer();
-    mockWebServer.start();
-    mockWebServer.setDispatcher(
-        new Dispatcher() {
-          @Override
-          public MockResponse dispatch(RecordedRequest request) {
-            switch (request.getPath()) {
-              case WEBTOON_URI:
-                return new MockResponse().setResponseCode(200).setBody("webtoon request ok");
-              case BGM_URI:
-                return new MockResponse().setResponseCode(200).setBody("bgm request ok");
-              case COMMENT_URI:
-                return new MockResponse().setResponseCode(200).setBody("comment request ok");
-              default:
-                return new MockResponse().setResponseCode(404);
-            }
-          }
-        });
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        mockWebServer.setDispatcher(
+                new Dispatcher() {
+                    @Override
+                    public MockResponse dispatch(RecordedRequest request) {
+                        switch (request.getPath()) {
+                            case WEBTOON_URI, BGM_URI:
+                                return new MockResponse().setHeadersDelay(300,
+                                        TimeUnit.MILLISECONDS);
+                            case COMMENT_URI:
+                                return new MockResponse().setResponseCode(200).setBody(AI_COMMENT);
+                            default:
+                                return new MockResponse().setResponseCode(404);
+                        }
+                    }
+                });
 
-    String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
-    WebClient aiWebClient =
-        WebClient.builder()
-            .baseUrl(baseUrl)
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .build();
-    aiService = new AiService(aiWebClient);
-  }
-
-  @AfterAll
-  static void tearDown() throws IOException {
-    mockWebServer.shutdown();
-  }
-
-  @Test
-  @DisplayName("WebClient가 실제로 비동기 API 요청을 보내는지 확인합니다.")
-  void webclientAsyncRequest() throws InterruptedException {
-    List<String> methodCallOrder = List.of(WEBTOON_URI, BGM_URI, COMMENT_URI);
-    List<String> requestOrder = List.of(WEBTOON_URI, BGM_URI, COMMENT_URI);
-    MemberEntity member = MemberEntity.builder().id(2L).build();
-    DiaryEntity diary =
-        DiaryEntity.builder()
-            .id(1L)
-            .emotion(DiaryEmotion.JOY)
-            .content("content")
-            .publicContent("public content")
-            .isPublic(true)
-            .memberEntity(member)
-            .build();
-
-    while (methodCallOrder.equals(requestOrder) == true) {
-      requestOrder = new ArrayList<>();
-      aiService.callAiContent(diary);
-
-      RecordedRequest request = mockWebServer.takeRequest();
-      requestOrder.add(request.getPath());
-      request = mockWebServer.takeRequest();
-      requestOrder.add(request.getPath());
-      request = mockWebServer.takeRequest();
-      requestOrder.add(request.getPath());
+        String baseUrl = String.format("http://localhost:%s", mockWebServer.getPort());
+        WebClient aiWebClient =
+                WebClient.builder()
+                        .baseUrl(baseUrl)
+                        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .build();
+        aiService = new AiService(aiWebClient);
     }
-    assertThat(methodCallOrder.equals(requestOrder)).isFalse();
-  }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    @DisplayName("AI 컨텐츠 요청 결과 확인")
+    void aiContentRequestTest() throws InterruptedException {
+        MemberEntity member = MemberEntity.builder().id(2L).build();
+        DiaryEntity diary =
+                DiaryEntity.builder()
+                        .id(1L)
+                        .emotion(DiaryEmotion.JOY)
+                        .content("content")
+                        .publicContent("public content")
+                        .isPublic(true)
+                        .memberEntity(member)
+                        .build();
+
+        AiContentResponse aiResponse = aiService.callAiContent(diary);
+        log.info("aiComment 결과 = {}", aiResponse.getAiComment());
+        Thread.sleep(300);
+        assertThat(aiResponse.getAiComment()).as("AI 코멘트 비동기 요청 응답이 올바르지 않습니다.")
+                .isEqualTo(AI_COMMENT);
+    }
 }
