ffed8b8cd232b59bd652ec5ce4122829195b1398
Sin Han <vrdhan212@gmail.com>
2024-10-22 08:40:46 +0900

    test: 시큐리티 의존성 제거

--- src/test/java/com/heartsave/todaktodak_api/auth/controller/AuthControllerTest.java
+++ src/test/java/com/heartsave/todaktodak_api/auth/controller/AuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.heartsave.todaktodak_api.auth.controller;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -8,29 +9,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heartsave.todaktodak_api.auth.dto.LoginIdCheckRequest;
 import com.heartsave.todaktodak_api.auth.dto.NicknameCheckRequest;
 import com.heartsave.todaktodak_api.auth.service.AuthService;
-import com.heartsave.todaktodak_api.common.security.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = AuthController.class)
-@Import(SecurityConfig.class)
 class AuthControllerTest {
   @Autowired private MockMvc mockMvc;
 
   @MockBean private AuthService authService;
-  @MockBean private AuthenticationEntryPoint authenticationEntryPoint;
 
   @Autowired ObjectMapper objectMapper;
 
   @Test
   @DisplayName("유일한 닉네임에 대한 중복 확인 요청")
+  @WithMockUser
   void checkNickname204Test() throws Exception {
     // given
     NicknameCheckRequest request = new NicknameCheckRequest("TEST_NICKNAME");
@@ -42,6 +40,7 @@ class AuthControllerTest {
     mockMvc
         .perform(
             post("/api/v1/auth/nickname")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isNoContent());
@@ -50,6 +49,7 @@ class AuthControllerTest {
 
   @Test
   @DisplayName("유일한 로그인 아이디에 대한 중복 확인 요청")
+  @WithMockUser
   void checkLoginId204Test() throws Exception {
     // given
     LoginIdCheckRequest request = new LoginIdCheckRequest("TEST_LOGIN_ID");
@@ -61,6 +61,7 @@ class AuthControllerTest {
     mockMvc
         .perform(
             post("/api/v1/auth/login-id")
+                .with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isNoContent());
