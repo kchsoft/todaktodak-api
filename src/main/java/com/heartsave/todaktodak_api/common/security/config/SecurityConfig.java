04643d4b3e107e20ed497fcd32e7f97312d70719
Sin Han <vrdhan212@gmail.com>
2024-10-21 10:59:46 +0900

    refactor: allowed origin 환경 변수화

--- src/main/java/com/heartsave/todaktodak_api/common/security/config/SecurityConfig.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/config/SecurityConfig.java
@@ -1,25 +1,36 @@
 package com.heartsave.todaktodak_api.common.security.config;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+  private final AuthenticationEntryPoint authenticationEntryPoint;
+
+  @Value("${todak.cors.allowed-origin}")
+  private List<String> ALLOWED_ORIGINS;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(AbstractHttpConfigurer::disable)
         .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
-        .exceptionHandling(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .exceptionHandling((eh) -> eh.authenticationEntryPoint(authenticationEntryPoint))
         .oauth2Login(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             authorize ->
@@ -45,8 +56,8 @@ public class SecurityConfig {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowCredentials(true);
     configuration.setAllowedMethods(List.of("HEAD", "OPTION", "GET", "POST", "PATCH", "DELETE"));
+    configuration.setAllowedOrigins(ALLOWED_ORIGINS);
     configuration.addAllowedHeader("*");
-    configuration.addAllowedOrigin("http://localhost:3000");
     return configuration;
   }
 }
