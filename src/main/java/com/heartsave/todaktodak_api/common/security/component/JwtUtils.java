4bdb1ee88e4297da33ca4f5beac426da7cc4bd23
Sin Han <vrdhan212@gmail.com>
2024-10-23 13:30:28 +0900

    feat: jwt 유틸리티

---
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/JwtUtils.java
@@ -0,0 +1,87 @@
+package com.heartsave.todaktodak_api.common.security.component;
+
+import com.heartsave.todaktodak_api.common.security.constant.JwtConstant;
+import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtils {
+  private final String ROLE = "role";
+  private final String TYPE = "type";
+
+  @Value("${jwt.secret-key}")
+  private String JWT_SECRET_KEY;
+
+  @Value("${jwt.access-expire-time}")
+  private Long ACCESS_TOKEN_EXPIRE_TIME_MILLI_SECOND;
+
+  @Value("${jwt.refresh-expire-time}")
+  private Long REFRESH_TOKEN_EXPIRE_TIME_MILLI_SECOND;
+
+  private SecretKey key;
+
+  public String issueToken(TodakUser user, String type) {
+    var now = System.currentTimeMillis();
+    var expireTime =
+        type.equals(JwtConstant.ACCESS_TYPE)
+            ? ACCESS_TOKEN_EXPIRE_TIME_MILLI_SECOND
+            : REFRESH_TOKEN_EXPIRE_TIME_MILLI_SECOND;
+    return Jwts.builder()
+        .subject(user.getUsername())
+        .claim(ROLE, user.getRole())
+        .claim(TYPE, type)
+        .issuedAt(new Date(now))
+        .expiration(new Date(now + expireTime))
+        .signWith(key)
+        .compact();
+  }
+
+  public Authentication getAuthentication(String token) {
+    TodakUser user = getUser(token);
+    return new UsernamePasswordAuthenticationToken(
+        extractSubject(token), "", user.getAuthorities());
+  }
+
+  private TodakUser getUser(String token) {
+    String role = extractRole(token);
+    return TodakUser.builder().role(role).build();
+  }
+
+  private String extractRole(String token) {
+    return extractAllClaims(token).get(ROLE, String.class);
+  }
+
+  private String extractType(String token) {
+    return extractAllClaims(token).get(TYPE, String.class);
+  }
+
+  private String extractSubject(String token) {
+    return extractAllClaims(token).getSubject();
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+  }
+
+  @PostConstruct
+  private void init() {
+    initKey();
+  }
+
+  private void initKey() {
+    var keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
+    key = Keys.hmacShaKeyFor(keyBytes);
+  }
+}
---
+++ src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.heartsave.todaktodak_api.common.security.constant;
+
+public class JwtConstant {
+  public static final String ACCESS_TYPE = "access";
+  public static final String REFRESH_TYPE = "refresh";
+  public static final String HEADER_PREFIX = "Bearer ";
+  public static final String REFRESH_KEY = "refresh_token";
+}
