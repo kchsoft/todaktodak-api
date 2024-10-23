26983339cd2d2c91a92f5fe43a2b3125aac8bc94
Sin Han <vrdhan212@gmail.com>
2024-10-23 20:45:45 +0900

    feat: 기본 OAuth2 설정 객체 TODAK-141

---
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/OAuth2SuccessHandler.java
@@ -0,0 +1,31 @@
+package com.heartsave.todaktodak_api.common.security.component;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+  @Override
+  public void onAuthenticationSuccess(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain chain,
+      Authentication authentication)
+      throws IOException, ServletException {
+    AuthenticationSuccessHandler.super.onAuthenticationSuccess(
+        request, response, chain, authentication);
+  }
+
+  @Override
+  public void onAuthenticationSuccess(
+      HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+      throws IOException, ServletException {}
+}
