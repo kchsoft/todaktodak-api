06e4e10ddb52b8f8964b552b8ffade8bdece3675
Sin Han <vrdhan212@gmail.com>
2024-10-23 16:58:55 +0900

    refactor: 액세스 토큰 헤더 키 상수화

--- src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
@@ -3,6 +3,7 @@ package com.heartsave.todaktodak_api.common.security.constant;
 public class JwtConstant {
   public static final String ACCESS_TYPE = "access";
   public static final String REFRESH_TYPE = "refresh";
-  public static final String HEADER_PREFIX = "Bearer ";
-  public static final String REFRESH_KEY = "refresh_token";
+  public static final String TOKEN_PREFIX = "Bearer ";
+  public static final String HEADER_KEY = "Authorization";
+  public static final String COOKIE_KEY = "refresh_token";
 }
