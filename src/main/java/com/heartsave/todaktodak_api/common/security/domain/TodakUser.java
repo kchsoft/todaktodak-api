eab839b6c562399de4beacaa7db9a3bf41a4ef37
Sin Han <vrdhan212@gmail.com>
2024-10-23 13:30:17 +0900

    fix: 인증 정보 권한 타입 변경

--- src/main/java/com/heartsave/todaktodak_api/common/security/domain/TodakUser.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/domain/TodakUser.java
@@ -1,7 +1,5 @@
 package com.heartsave.todaktodak_api.common.security.domain;
 
-import com.heartsave.todaktodak_api.member.domain.TodakRole;
-import com.heartsave.todaktodak_api.member.entity.MemberEntity;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Collections;
@@ -14,18 +12,15 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Builder
 public class TodakUser implements UserDetails, OAuth2User, Serializable {
 
   private final Long id;
   private final String username;
-  private final TodakRole role;
+  private final String role;
 
   // TODO: 각 소셜 플랫폼 적용
   private final Map<String, Object> attributes;
-
-  public static TodakUser from(MemberEntity entity) {
-    return new TodakUser(entity.getId(), entity.getLoginId(), entity.getRole(), Map.of());
-  }
 
   @Override
   public Map<String, Object> getAttributes() {
@@ -34,7 +29,7 @@ public class TodakUser implements UserDetails, OAuth2User, Serializable {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    return Collections.singletonList(new SimpleGrantedAuthority(role));
   }
 
   @Override
