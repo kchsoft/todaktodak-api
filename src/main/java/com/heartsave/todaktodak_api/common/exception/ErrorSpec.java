e0c66fb0c1454d7c5f23396fdd7f59db48054914
Sin Han <vrdhan212@gmail.com>
2024-10-24 20:41:17 +0900

    refactor: 로그인 실패에 대한 에러 명세 추가 TODAK-141

--- src/main/java/com/heartsave/todaktodak_api/common/exception/ErrorSpec.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/ErrorSpec.java
@@ -11,7 +11,8 @@ public enum ErrorSpec {
   LIMIT_EXCEED(HttpStatus.BAD_REQUEST, "최대 횟수를 초과했습니다."),
   NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않습니다."),
   EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다.");
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+  INCORRECT_USERNAME_PASSWORD(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 틀렸습니다.");
 
   private final HttpStatus status;
   private final String description;
