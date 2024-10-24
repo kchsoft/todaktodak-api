81829a24c67261ef2d2e501314a47f9139be90e5
Sin Han <vrdhan212@gmail.com>
2024-10-24 09:56:54 +0900

    refactor: 토큰 유효성 결과에 따른 에러 응답 세분화 TODAK-127

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/JwtValidationFilter.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/JwtValidationFilter.java
@@ -39,50 +39,46 @@ public class JwtValidationFilter extends OncePerRequestFilter {
       throws ServletException, IOException {
     var token = extractToken(request);
 
-    if (!isValidToken(token)) {
-      logger.error("INVALID TOKEN: {}", token);
-      setErrorResponse(response);
+    if (token == null) {
+      logger.error("NO TOKEN: {}", token);
+      setErrorResponse(response, ErrorSpec.INVALID_TOKEN);
       return;
     }
+    try {
+      jwtUtils.extractAllClaims(token);
 
-    setAuthentication(token);
-    filterChain.doFilter(request, response);
+      if (!isValidTokenType(token)) {
+        logger.error("유효하지 않은 토큰 유형입니다.");
+        setErrorResponse(response, ErrorSpec.INVALID_TOKEN);
+        return;
+      }
+      setAuthentication(token);
+      filterChain.doFilter(request, response);
+    } catch (ExpiredJwtException e) {
+      logger.error("토큰이 만료됐습니다.");
+      setErrorResponse(response, ErrorSpec.EXPIRED_TOKEN);
+    } catch (SecurityException
+        | MalformedJwtException
+        | UnsupportedJwtException
+        | IllegalArgumentException e) {
+      logger.error("유효하지 않은 토큰입니다.");
+      setErrorResponse(response, ErrorSpec.INVALID_TOKEN);
+    }
   }
 
-  private void setErrorResponse(HttpServletResponse response) throws IOException {
+  private void setErrorResponse(HttpServletResponse response, ErrorSpec errorSpec)
+      throws IOException {
     response.setStatus(HttpStatus.UNAUTHORIZED.value());
     response.setContentType("application/json;charset=UTF-8");
     response
         .getWriter()
-        .write(objectMapper.writeValueAsString(ErrorResponse.from(ErrorSpec.EXPIRED_TOKEN)));
+        .write(objectMapper.writeValueAsString(ErrorResponse.from(errorSpec, "로그인이 실패됐습니다.")));
   }
 
   private String extractToken(HttpServletRequest request) {
     String value = request.getHeader(JwtConstant.HEADER_KEY);
     if (value == null || !value.contains(JwtConstant.TOKEN_PREFIX)) return null;
     return value.substring(JwtConstant.TOKEN_PREFIX.length());
-  }
-
-  private boolean isValidToken(String token) {
-    return token != null && isValidTokenStructure(token) && isValidTokenType(token);
-  }
-
-  private boolean isValidTokenStructure(String token) {
-    try {
-      jwtUtils.extractAllClaims(token);
-      return true;
-    } catch (SecurityException e) {
-      logger.error("서명이 유효하지 않습니다.");
-    } catch (MalformedJwtException e) {
-      logger.error("잘못된 JWT 형식입니다.");
-    } catch (UnsupportedJwtException e) {
-      logger.error("지원하지 않는 JWT입니다.");
-    } catch (ExpiredJwtException e) {
-      logger.error("토큰이 만료됐습니다.");
-    } catch (IllegalArgumentException e) {
-      logger.error("토큰이 비었거나 존재하지 않습니다.");
-    }
-    return false;
   }
 
   private boolean isValidTokenType(String token) {
