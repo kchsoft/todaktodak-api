7c2c7c824b0e18cf4e7785637e3e1c44ed65913b
Sin Han <vrdhan212@gmail.com>
2024-10-24 11:27:12 +0900

    feat: jwt 로그인 수행 필터 TODAK-141

---
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/jwt/JwtAuthFilter.java
@@ -0,0 +1,45 @@
+package com.heartsave.todaktodak_api.common.security.component.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
+  private final AuthenticationManager authenticationManager;
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Override
+  public Authentication attemptAuthentication(
+      HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    return super.attemptAuthentication(request, response);
+  }
+
+  @Override
+  protected void successfulAuthentication(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain chain,
+      Authentication authResult)
+      throws IOException, ServletException {
+    super.successfulAuthentication(request, response, chain, authResult);
+  }
+
+  @Override
+  protected void unsuccessfulAuthentication(
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException failed)
+      throws IOException, ServletException {
+    super.unsuccessfulAuthentication(request, response, failed);
+  }
+}
