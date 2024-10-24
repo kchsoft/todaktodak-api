5697739b1d5dc27dd48c2151564908abdaac9b87
Sin Han <vrdhan212@gmail.com>
2024-10-24 16:11:24 +0900

    refactor: 로그인 요청 url 설정 TODAK-141

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/jwt/JwtAuthFilter.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/jwt/JwtAuthFilter.java
@@ -1,11 +1,11 @@
 package com.heartsave.todaktodak_api.common.security.component.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,10 +13,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@RequiredArgsConstructor
 public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
   private final AuthenticationManager authenticationManager;
+  private final ObjectMapper objectMapper;
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  public JwtAuthFilter(AuthenticationManager authenticationManager, ObjectMapper objectMapper) {
+    this.authenticationManager = authenticationManager;
+    this.objectMapper = objectMapper;
+    setFilterProcessesUrl("/api/v1/auth/login");
+  }
 
   @Override
   public Authentication attemptAuthentication(
