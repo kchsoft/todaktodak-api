10a9766451b91c8234f19e1d9b63e99d5ee34164
Sin Han <vrdhan212@gmail.com>
2024-10-24 20:59:31 +0900

    test: 컨트롤러 테스트에 인증 필터 스캔 비활성화 TODAK-141

--- src/test/java/com/heartsave/todaktodak_api/config/TestSecurityConfig.java
+++ src/test/java/com/heartsave/todaktodak_api/config/TestSecurityConfig.java
@@ -1,8 +1,6 @@
 package com.heartsave.todaktodak_api.config;
 
-import com.heartsave.todaktodak_api.common.security.component.jwt.JwtValidationFilter;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class TestSecurityConfig {
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
         .csrf(AbstractHttpConfigurer::disable) // 테스트에서는 CSRF 비활성화
         .cors(AbstractHttpConfigurer::disable)
@@ -22,6 +20,4 @@ public class TestSecurityConfig {
         .httpBasic(AbstractHttpConfigurer::disable)
         .build();
   }
-
-  @MockBean private JwtValidationFilter jwtValidationFilter;
 }
