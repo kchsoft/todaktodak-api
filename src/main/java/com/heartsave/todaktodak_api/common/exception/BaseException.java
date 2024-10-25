822b635447db97e70de14cb0e43f9532e0166f4e
kchranger <kchranger@gmail.com>
2024-10-25 09:15:12 +0900

    refactor: ErrorField에 빌드 패턴 적용 TODAK-152

--- src/main/java/com/heartsave/todaktodak_api/common/exception/BaseException.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/BaseException.java
@@ -2,16 +2,19 @@ package com.heartsave.todaktodak_api.common.exception;
 
 import com.heartsave.todaktodak_api.common.exception.errorspec.ErrorSpec;
 import java.util.StringJoiner;
+import lombok.AccessLevel;
 import lombok.Getter;
 
 @Getter
 public abstract class BaseException extends RuntimeException {
   private final ErrorSpec errorSpec;
+
+  @Getter(AccessLevel.NONE)
   private final ErrorField errorField;
 
-  protected BaseException(ErrorSpec errorSpec) {
+  protected BaseException(ErrorSpec errorSpec, ErrorField errorField) {
     this.errorSpec = errorSpec;
-    this.errorField = new ErrorField();
+    this.errorField = errorField;
   }
 
   public String getLogMessage() {
--- src/main/java/com/heartsave/todaktodak_api/diary/exception/DiaryException.java
+++ src/main/java/com/heartsave/todaktodak_api/diary/exception/DiaryException.java
@@ -1,6 +1,7 @@
 package com.heartsave.todaktodak_api.diary.exception;
 
 import com.heartsave.todaktodak_api.common.exception.BaseException;
+import com.heartsave.todaktodak_api.common.exception.ErrorFieldBuilder;
 import com.heartsave.todaktodak_api.common.exception.errorspec.DiaryErrorSpec;
 import lombok.Getter;
 
@@ -8,12 +9,12 @@ import lombok.Getter;
 public abstract class DiaryException extends BaseException {
 
   protected DiaryException(DiaryErrorSpec errorSpec, Long memberId, Long diaryId) {
-    super(errorSpec);
-    getErrorField().add("memberId", memberId).add("diaryId", diaryId);
+    super(
+        errorSpec,
+        ErrorFieldBuilder.builder().add("memberId", memberId).add("diaryId", diaryId).build());
   }
 
   protected DiaryException(DiaryErrorSpec errorSpec, Long memberId) {
-    super(errorSpec);
-    getErrorField().add("memberId", memberId);
+    super(errorSpec, ErrorFieldBuilder.builder().add("memberId", memberId).build());
   }
 }
