b5e7cc5c14c4977e925b74634506e2578d833d42
Sin Han <vrdhan212@gmail.com>
2024-10-29 19:01:36 +0900

    feat: OAuth2 로그인 실패 처리 TODAK-154

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/oauth2/Oauth2FailureHandler.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/oauth2/Oauth2FailureHandler.java
@@ -1,10 +1,12 @@
 package com.heartsave.todaktodak_api.common.security.component.oauth2;
 
-import jakarta.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heartsave.todaktodak_api.common.exception.errorspec.AuthErrorSpec;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -12,8 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class Oauth2FailureHandler implements AuthenticationFailureHandler {
+  private final ObjectMapper objectMapper;
+
   @Override
   public void onAuthenticationFailure(
       HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-      throws IOException, ServletException {}
+      throws IOException {
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.setContentType("application/json;charset=UTF-8");
+    response.getWriter().write(objectMapper.writeValueAsString(AuthErrorSpec.OAUTH_LOGIN_FAIL));
+  }
 }
