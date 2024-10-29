70e3b2d8a01ca9ef4148ee696fb5ca79473a56fc
Sin Han <vrdhan212@gmail.com>
2024-10-29 19:01:27 +0900

    feat: OAuth2 실패 에러 명세 TODAK-154

--- src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/AuthErrorSpec.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/AuthErrorSpec.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorSpec implements ErrorSpec {
   INCORRECT_USERNAME_PASSWORD(
       HttpStatus.UNAUTHORIZED, "AUTH-001", "아이디 또는 비밀번호가 틀렸습니다.", "로그인이 실패됐습니다."),
-  DUPLICATED_INFORMATION(HttpStatus.CONFLICT, "AUTH-002", "중복된 정보가 있습니다.", "회원가입 요청 실패");
+  DUPLICATED_INFORMATION(HttpStatus.CONFLICT, "AUTH-002", "중복된 정보가 있습니다.", "회원가입 요청 실패"),
+  OAUTH_LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "AUTH-003", "소셜 로그인이 실패됐습니다.", "OAuth2 로그인 실패");
 
   private final HttpStatus status;
   private final String code;
