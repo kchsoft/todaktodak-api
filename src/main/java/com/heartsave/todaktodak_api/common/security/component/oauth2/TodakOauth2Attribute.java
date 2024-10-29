05001b9619e145a8b667c62b0f9d1ddf63355845
Sin Han <vrdhan212@gmail.com>
2024-10-29 17:50:34 +0900

    refactor: oauth2 리소스에 인증 유형 추가 TODAK-154

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/oauth2/TodakOauth2Attribute.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/oauth2/TodakOauth2Attribute.java
@@ -2,11 +2,15 @@ package com.heartsave.todaktodak_api.common.security.component.oauth2;
 
 import static com.heartsave.todaktodak_api.common.security.constant.ConstraintConstant.Member.LOGIN_ID_MAX_SIZE;
 
+import com.heartsave.todaktodak_api.common.security.domain.AuthType;
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 // 소셜 로그인별로 정형화된 형식의 리소스로 가공
 @Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class TodakOauth2Attribute {
   private static final String NAVER = "naver";
   private static final String KAKAO = "kakao";
@@ -14,11 +18,7 @@ public class TodakOauth2Attribute {
 
   private final String username;
   private final String email;
-
-  private TodakOauth2Attribute(String username, String email) {
-    this.username = username;
-    this.email = email;
-  }
+  private final AuthType authType;
 
   //
   public static TodakOauth2Attribute of(Map<String, Object> attributes, String authType) {
@@ -35,7 +35,7 @@ public class TodakOauth2Attribute {
     String id = String.valueOf(attributes.get("id")); // Long -> String 변환
     Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
     String email = (String) kakaoAccount.get("email");
-    return new TodakOauth2Attribute(id + "_" + KAKAO, email);
+    return new TodakOauth2Attribute(id + "_" + KAKAO, email, AuthType.KAKAO);
   }
 
   @SuppressWarnings("unchecked")
@@ -44,11 +44,18 @@ public class TodakOauth2Attribute {
     String id = (String) response.get("id");
     id = id.substring(0, Math.min(id.length(), LOGIN_ID_MAX_SIZE - 6));
     String email = (String) response.get("email");
-    return new TodakOauth2Attribute(id + "_" + NAVER, email);
+    return new TodakOauth2Attribute(id + "_" + NAVER, email, AuthType.NAVER);
   }
 
   private static TodakOauth2Attribute ofGoogle(Map<String, Object> attributes) {
     return new TodakOauth2Attribute(
-        attributes.get("sub") + "_" + GOOGLE, (String) attributes.get("email"));
+        attributes.get("sub") + "_" + GOOGLE, (String) attributes.get("email"), AuthType.GOOGLE);
+  }
+
+  @Override
+  public String toString() {
+    return """
+USERNAME: %s, EMAIL: %s, AUTH TYPE: %s"""
+        .formatted(username, email, authType.name());
   }
 }
