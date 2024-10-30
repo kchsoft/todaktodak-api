b14a94c921b0a9f639e9b6920f856548e368ed95
Sin Han <vrdhan212@gmail.com>
2024-10-30 14:53:13 +0900

    feat: otp 관련 에러 명세 TODAK-173

--- src/main/java/com/heartsave/todaktodak_api/auth/exception/AuthException.java
+++ src/main/java/com/heartsave/todaktodak_api/auth/exception/AuthException.java
@@ -6,6 +6,10 @@ import com.heartsave.todaktodak_api.common.exception.ErrorFieldBuilder;
 import com.heartsave.todaktodak_api.common.exception.errorspec.ErrorSpec;
 
 public final class AuthException extends BaseException {
+  public AuthException(ErrorSpec errorSpec) {
+    super(errorSpec, null);
+  }
+
   public AuthException(ErrorSpec errorSpec, SignUpRequest dto) {
     super(
         errorSpec,
