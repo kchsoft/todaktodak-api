9d3beb92bd0cd70ff6821d009ae9679b0f02bd58
Sin Han <vrdhan212@gmail.com>
2024-10-31 22:01:17 +0900

    feat: 토큰 재발급 실패에 대한 에러 명세 TODAK-175

--- src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/TokenErrorSpec.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/TokenErrorSpec.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum TokenErrorSpec implements ErrorSpec {
-  EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-001", "로그인 정보가 만료되었습니다.", "회원의 토큰이 만료되었습니다."),
-  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-002", "유효하지 않은 로그인 정보 입니다.", "회원의 토큰이 유효하지 않습니다."),
-  NON_EXISTENT_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-003", "로그인이 필요합니다.", "액세스 토큰이 없는 요청입니다.");
+  EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-001", "로그인 정보가 만료되었습니다.", "액세스 토큰이 만료되었습니다."),
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-002", "비정상 로그인 시도입니다.", "유효하지 않은 토큰으로 인증 시도"),
+  NON_EXISTENT_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-003", "로그인 정보가 만료되었습니다.", "액세스 토큰이 없는 요청입니다.");
 
   private final HttpStatus status;
   private final String code;
