bd304723652a2fa2c3526f61daf48531825929be
Sin Han <vrdhan212@gmail.com>
2024-10-31 21:20:53 +0900

    refactor: 토큰 에러 응답 세분화 TODAK-175
    유효성 검사 실패, 비정상 접근, 새로고침 직후 요청

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/AuthenticationEntryPointImpl.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/AuthenticationEntryPointImpl.java
@@ -1,13 +1,18 @@
 package com.heartsave.todaktodak_api.common.security.component;
 
+import static com.heartsave.todaktodak_api.common.security.constant.JwtConstant.NO_TOKEN_REQUEST_ATTRIBUTE_KEY;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heartsave.todaktodak_api.common.exception.ErrorResponse;
 import com.heartsave.todaktodak_api.common.exception.errorspec.AuthErrorSpec;
+import com.heartsave.todaktodak_api.common.exception.errorspec.ErrorSpec;
 import com.heartsave.todaktodak_api.common.exception.errorspec.TokenErrorSpec;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -18,6 +23,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
   private final ObjectMapper objectMapper;
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Override
   public void commence(
@@ -25,22 +31,39 @@ public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
       HttpServletResponse response,
       AuthenticationException authException)
       throws IOException {
-    setResponseWithMessage(response, authException);
+    setResponseWithMessage(request, response, authException);
   }
 
-  private void setResponseWithMessage(HttpServletResponse response, AuthenticationException e)
+  private void setResponseWithMessage(
+      HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
       throws IOException {
+    response.setCharacterEncoding("UTF-8");
     response.setStatus(HttpStatus.UNAUTHORIZED.value());
     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
-    // 토큰 관련 에러 발생 기대
+    // 토큰이 없는 요청에 대한 응답
+    if (isNoTokenRequest(request)) {
+      response
+          .getWriter()
+          .write(
+              objectMapper.writeValueAsString(
+                  ErrorResponse.from(
+                      (ErrorSpec) request.getAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY))));
+      return;
+    }
+
+    // 토큰 유효성 검사 실패에 대한 응답
     try {
       TokenErrorSpec spec = TokenErrorSpec.valueOf(e.getMessage());
       response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.from(spec)));
-    } catch (IllegalArgumentException ex) {
+    } catch (Exception ex) {
       response
           .getWriter()
           .write(objectMapper.writeValueAsString(ErrorResponse.from(AuthErrorSpec.AUTH_FAIL)));
     }
+  }
+
+  private boolean isNoTokenRequest(HttpServletRequest request) {
+    return request.getAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY) != null;
   }
 }
