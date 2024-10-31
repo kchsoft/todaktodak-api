513cc1b62c18f99dacf7e9ce3534282b092526b6
Sin Han <vrdhan212@gmail.com>
2024-10-31 10:53:36 +0900

    feat: 유효하지 않은 토큰 에러 설명 TODAK-175

--- src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
@@ -6,4 +6,5 @@ public class JwtConstant {
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_KEY = "Authorization";
   public static final String REFRESH_TOKEN_COOKIE_KEY = "refresh_token";
+  public static final String INVALID_TOKEN_ERROR_DETAIL = "INVALID TOKEN ERROR";
 }
