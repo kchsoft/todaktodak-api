8e1b7663efb21fcc73e7dc889bb7306bc02de920
Sin Han <vrdhan212@gmail.com>
2024-10-31 22:45:47 +0900

    style: 토큰 유효성 검사 로직 클린 코드 TODAK-175

--- src/main/java/com/heartsave/todaktodak_api/common/security/component/jwt/JwtValidationFilter.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/component/jwt/JwtValidationFilter.java
@@ -6,7 +6,6 @@ import static com.heartsave.todaktodak_api.common.security.util.JwtUtils.*;
 import com.heartsave.todaktodak_api.common.exception.errorspec.AuthErrorSpec;
 import com.heartsave.todaktodak_api.common.exception.errorspec.TokenErrorSpec;
 import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
-import com.heartsave.todaktodak_api.common.security.util.JwtUtils;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
@@ -42,42 +41,14 @@ public class JwtValidationFilter extends OncePerRequestFilter {
     var token = extractToken(request);
 
     if (token == null) {
-      setInvalidRequestAttribute(request);
+      setNoTokenRequestAttribute(request);
       filterChain.doFilter(request, response);
       return;
     }
-    try {
-      JwtUtils.extractAllClaims(token);
-      if (!isValidTokenType(token)) {
-        logger.error("유효하지 않은 토큰 유형입니다. {}", token);
-        authenticationEntryPoint.commence(
-            request, response, new BadCredentialsException(TokenErrorSpec.INVALID_TOKEN.name()));
-      } else {
-        setAuthentication(token);
-        filterChain.doFilter(request, response);
-      }
-    } catch (ExpiredJwtException e) {
-      logger.error("토큰이 만료됐습니다. {}", token);
-      authenticationEntryPoint.commence(
-          request, response, new BadCredentialsException(TokenErrorSpec.EXPIRED_TOKEN.name()));
-    } catch (SecurityException
-        | MalformedJwtException
-        | UnsupportedJwtException
-        | IllegalArgumentException e) {
-      logger.error("유효하지 않은 토큰입니다. {}", token);
-      authenticationEntryPoint.commence(
-          request, response, new BadCredentialsException(TokenErrorSpec.INVALID_TOKEN.name()));
-    }
-  }
 
-  private void setInvalidRequestAttribute(HttpServletRequest request) {
-    if (request.getCookies() != null
-        && Arrays.stream(request.getCookies())
-            .anyMatch(p -> p.getName().equals(REFRESH_TOKEN_COOKIE_KEY))) {
-      request.setAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY, TokenErrorSpec.NON_EXISTENT_TOKEN);
-    } else {
-      request.setAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY, AuthErrorSpec.ABNORMAL_ACCESS);
-    }
+    if (!validateTokenAndProcess(token, request, response)) return;
+    setAuthentication(token);
+    filterChain.doFilter(request, response);
   }
 
   @Nullable
@@ -87,8 +58,52 @@ public class JwtValidationFilter extends OncePerRequestFilter {
     return value.substring(TOKEN_PREFIX.length());
   }
 
-  private boolean isValidTokenType(String token) {
-    return JwtUtils.extractType(token).equals(ACCESS_TYPE);
+  private void setNoTokenRequestAttribute(HttpServletRequest request) {
+    if (existRefreshTokenCookie(request)) {
+      request.setAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY, TokenErrorSpec.NON_EXISTENT_TOKEN);
+    } else {
+      request.setAttribute(NO_TOKEN_REQUEST_ATTRIBUTE_KEY, AuthErrorSpec.ABNORMAL_ACCESS);
+    }
+  }
+
+  private boolean existRefreshTokenCookie(HttpServletRequest request) {
+    return request.getCookies() != null
+        && Arrays.stream(request.getCookies())
+            .anyMatch(p -> p.getName().equals(REFRESH_TOKEN_COOKIE_KEY));
+  }
+
+  private boolean validateTokenAndProcess(
+      String token, HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    try {
+      extractAllClaims(token);
+      if (!isAccessTokenType(token)) {
+        logger.error("유효하지 않은 토큰 유형입니다. {}", token);
+        respondAuthError(request, response, TokenErrorSpec.INVALID_TOKEN.name());
+        return false;
+      }
+      return true;
+    } catch (ExpiredJwtException e) {
+      logger.error("토큰이 만료됐습니다. {}", token);
+      respondAuthError(request, response, TokenErrorSpec.EXPIRED_TOKEN.name());
+    } catch (SecurityException
+        | MalformedJwtException
+        | UnsupportedJwtException
+        | IllegalArgumentException e) {
+      logger.error("유효하지 않은 토큰입니다. {}", token);
+      respondAuthError(request, response, TokenErrorSpec.INVALID_TOKEN.name());
+    }
+    return false;
+  }
+
+  private boolean isAccessTokenType(String token) {
+    return extractType(token).equals(ACCESS_TYPE);
+  }
+
+  private void respondAuthError(
+      HttpServletRequest request, HttpServletResponse response, String errorMessage)
+      throws ServletException, IOException {
+    authenticationEntryPoint.commence(request, response, new BadCredentialsException(errorMessage));
   }
 
   private void setAuthentication(String token) {
