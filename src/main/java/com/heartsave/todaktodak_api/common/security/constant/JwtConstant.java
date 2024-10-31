9af7aecaa24d5ece8f8e0cc31df90e94483d6938
Sin Han <vrdhan212@gmail.com>
2024-10-31 11:19:50 +0900

    remove: 불필요 에러 설명 상수 TODAK-175

--- src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
+++ src/main/java/com/heartsave/todaktodak_api/common/security/constant/JwtConstant.java
@@ -6,5 +6,4 @@ public class JwtConstant {
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_KEY = "Authorization";
   public static final String REFRESH_TOKEN_COOKIE_KEY = "refresh_token";
-  public static final String INVALID_TOKEN_ERROR_DETAIL = "INVALID TOKEN ERROR";
 }
