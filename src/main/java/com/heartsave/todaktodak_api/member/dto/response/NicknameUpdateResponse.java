c78c465787359e90da06cbae9a1de5f710f47de1
Sin Han <vrdhan212@gmail.com>
2024-11-01 15:09:37 +0900

    feat: 회원 프로필에 대해서 DB 조회 TODAK-186

--- src/main/java/com/heartsave/todaktodak_api/member/dto/response/NicknameUpdateResponse.java
+++ src/main/java/com/heartsave/todaktodak_api/member/dto/response/NicknameUpdateResponse.java
@@ -4,5 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "닉네임 변경 API 응답 객체")
 public record NicknameUpdateResponse(
     @Schema(example = "todak", description = "변경된 닉네임") String nickname) {}
--- src/main/java/com/heartsave/todaktodak_api/member/repository/MemberRepository.java
+++ src/main/java/com/heartsave/todaktodak_api/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.heartsave.todaktodak_api.member.repository;
 
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
+import com.heartsave.todaktodak_api.member.entity.projection.MemberProfileProjection;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,6 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
   Optional<MemberEntity> findMemberEntityByEmail(String email);
 
   boolean existsByEmail(String email);
+
+  Optional<MemberProfileProjection> findProjectedById(Long id);
 }
