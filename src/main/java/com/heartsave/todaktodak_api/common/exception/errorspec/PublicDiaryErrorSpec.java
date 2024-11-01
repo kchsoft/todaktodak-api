1fcbaa9b586599174fb43a1c40066cdc84ea8d56
kchranger <kchranger@gmail.com>
2024-11-01 15:47:34 +0900

    refactor: 공개 일기 예외 발생 http status 변경 TODAK-174

--- src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/PublicDiaryErrorSpec.java
+++ src/main/java/com/heartsave/todaktodak_api/common/exception/errorspec/PublicDiaryErrorSpec.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum PublicDiaryErrorSpec implements ErrorSpec {
   PUBLIC_DIARY_NOT_FOUND(
-      HttpStatus.BAD_REQUEST,
+      HttpStatus.NOT_FOUND,
       "PUBLIC_DIARY-001",
       "공개 일기를 찾을 수 없습니다.",
       "다음의 공개 일기 목록을 DB에서 조회할 수 없습니다.");
