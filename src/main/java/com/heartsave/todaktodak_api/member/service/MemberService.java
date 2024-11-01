ae278ace6573336a99132be9b23082246cadb2a9
Sin Han <vrdhan212@gmail.com>
2024-11-01 14:04:37 +0900

    feat: 회원 닉네임 변경 처리 TODAK-186

--- src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
+++ src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
@@ -1,3 +1,32 @@
 package com.heartsave.todaktodak_api.member.service;
 
-public interface MemberService {}
+import com.heartsave.todaktodak_api.common.exception.errorspec.MemberErrorSpec;
+import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
+import com.heartsave.todaktodak_api.member.dto.request.NicknameUpdateRequest;
+import com.heartsave.todaktodak_api.member.dto.response.NicknameUpdateResponse;
+import com.heartsave.todaktodak_api.member.entity.MemberEntity;
+import com.heartsave.todaktodak_api.member.exception.MemberNotFoundException;
+import com.heartsave.todaktodak_api.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+  private final MemberRepository memberRepository;
+
+  @Transactional
+  public NicknameUpdateResponse updateNickname(TodakUser user, NicknameUpdateRequest dto) {
+    MemberEntity retrievedMember = findMemberById(user.getId());
+    retrievedMember.updateNickname(dto.nickname());
+    return NicknameUpdateResponse.builder().nickname(retrievedMember.getNickname()).build();
+  }
+
+  private MemberEntity findMemberById(Long id) {
+    return memberRepository
+        .findById(id)
+        .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND, id));
+  }
+}
