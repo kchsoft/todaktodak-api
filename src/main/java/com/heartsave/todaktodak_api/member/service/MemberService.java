d98aaf30a6ee8c0f158bb284d5c21ac90f87aa5f
Sin Han <vrdhan212@gmail.com>
2024-11-01 15:09:47 +0900

    feat: 회원 프로필 조회 로직 TODAK-186

--- src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
+++ src/main/java/com/heartsave/todaktodak_api/member/service/MemberService.java
@@ -3,11 +3,15 @@ package com.heartsave.todaktodak_api.member.service;
 import com.heartsave.todaktodak_api.common.exception.errorspec.MemberErrorSpec;
 import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
 import com.heartsave.todaktodak_api.member.dto.request.NicknameUpdateRequest;
+import com.heartsave.todaktodak_api.member.dto.response.MemberProfileResponse;
 import com.heartsave.todaktodak_api.member.dto.response.NicknameUpdateResponse;
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
+import com.heartsave.todaktodak_api.member.entity.projection.MemberProfileProjection;
 import com.heartsave.todaktodak_api.member.exception.MemberNotFoundException;
 import com.heartsave.todaktodak_api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,13 +19,35 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
   private final MemberRepository memberRepository;
 
   @Transactional
-  public NicknameUpdateResponse updateNickname(TodakUser user, NicknameUpdateRequest dto) {
-    MemberEntity retrievedMember = findMemberById(user.getId());
+  public NicknameUpdateResponse updateNickname(TodakUser principal, NicknameUpdateRequest dto) {
+    MemberEntity retrievedMember = findMemberById(principal.getId());
     retrievedMember.updateNickname(dto.nickname());
     return NicknameUpdateResponse.builder().nickname(retrievedMember.getNickname()).build();
+  }
+
+  public MemberProfileResponse getMemberProfile(TodakUser principal) {
+    MemberProfileProjection memberProfile = getMemberProfile(principal.getId());
+    logger.info("프로필: {}", memberProfile);
+    // TODO: presigned url 생성
+    String characterPreSignedUrl =
+        memberProfile.getCharacterImageUrl() == null
+            ? "TODO"
+            : memberProfile.getCharacterImageUrl();
+    return MemberProfileResponse.builder()
+        .nickname(memberProfile.getNickname())
+        .email(memberProfile.getEmail())
+        .characterImageUrl(characterPreSignedUrl)
+        .build();
+  }
+
+  private MemberProfileProjection getMemberProfile(Long id) {
+    return memberRepository
+        .findProjectedById(id)
+        .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND, id));
   }
 
   private MemberEntity findMemberById(Long id) {
