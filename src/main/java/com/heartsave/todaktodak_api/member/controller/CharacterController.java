4476955d5c21d038f6929fb642a5d414f62f6332
Sin Han <vrdhan212@gmail.com>
2024-11-04 17:09:03 +0900

    feat: 캐릭터 생성 및 등록 API TODAK-201

--- src/main/java/com/heartsave/todaktodak_api/member/controller/CharacterController.java
+++ src/main/java/com/heartsave/todaktodak_api/member/controller/CharacterController.java
@@ -1,17 +1,19 @@
 package com.heartsave.todaktodak_api.member.controller;
 
+import com.heartsave.todaktodak_api.ai.dto.request.AiCharacterRequest;
 import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
+import com.heartsave.todaktodak_api.member.dto.response.CharacterRegisterResponse;
 import com.heartsave.todaktodak_api.member.dto.response.CharacterTemporaryImageResponse;
 import com.heartsave.todaktodak_api.member.service.CharacterService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "회원 캐릭터", description = "회원 캐릭터 관련 API")
 @RestController
@@ -29,5 +31,30 @@ public class CharacterController {
   public ResponseEntity<CharacterTemporaryImageResponse> getPastCharacterImage(
       @AuthenticationPrincipal TodakUser principal) {
     return ResponseEntity.ok(characterService.getPastCharacterImage(principal));
+  }
+
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "기존 캐릭터 이미지 경로 응답"),
+        @ApiResponse(responseCode = "404", description = "회원 조회 실패")
+      })
+  @PostMapping
+  public ResponseEntity<Void> getPastCharacterImage(
+      @AuthenticationPrincipal TodakUser principal,
+      @RequestParam("uploadImage") MultipartFile file,
+      AiCharacterRequest dto) {
+    characterService.createCharacterImage(principal, file, dto);
+    return ResponseEntity.noContent().build();
+  }
+
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "캐릭터 등록 완료 및 회원 권한 승격"),
+        @ApiResponse(responseCode = "404", description = "회원 조회 실패")
+      })
+  @PostMapping("/register")
+  public ResponseEntity<CharacterRegisterResponse> completeCharacterRegister(
+      @AuthenticationPrincipal TodakUser principal, HttpServletResponse response) {
+    return ResponseEntity.ok(characterService.changeRoleAndReissueToken(principal, response));
   }
 }
--- src/main/java/com/heartsave/todaktodak_api/member/service/CharacterService.java
+++ src/main/java/com/heartsave/todaktodak_api/member/service/CharacterService.java
@@ -1,34 +1,79 @@
 package com.heartsave.todaktodak_api.member.service;
 
+import static com.heartsave.todaktodak_api.common.security.constant.JwtConstant.REFRESH_TOKEN_COOKIE_KEY;
+
+import com.heartsave.todaktodak_api.ai.dto.request.AiCharacterRequest;
+import com.heartsave.todaktodak_api.ai.service.AiService;
 import com.heartsave.todaktodak_api.common.exception.errorspec.MemberErrorSpec;
+import com.heartsave.todaktodak_api.common.security.constant.JwtConstant;
 import com.heartsave.todaktodak_api.common.security.domain.TodakUser;
+import com.heartsave.todaktodak_api.common.security.util.CookieUtils;
+import com.heartsave.todaktodak_api.common.security.util.JwtUtils;
 import com.heartsave.todaktodak_api.common.storage.S3FileStorageService;
+import com.heartsave.todaktodak_api.member.domain.TodakRole;
+import com.heartsave.todaktodak_api.member.dto.response.CharacterRegisterResponse;
 import com.heartsave.todaktodak_api.member.dto.response.CharacterTemporaryImageResponse;
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
 import com.heartsave.todaktodak_api.member.exception.MemberNotFoundException;
 import com.heartsave.todaktodak_api.member.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CharacterService {
   private final MemberRepository memberRepository;
+  private final AiService aiService;
   private final S3FileStorageService s3Service;
 
   @Transactional(readOnly = true)
   public CharacterTemporaryImageResponse getPastCharacterImage(TodakUser principal) {
-    Long id = principal.getId();
-    MemberEntity retrievedMember =
-        memberRepository
-            .findById(id)
-            .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND, id));
-
+    MemberEntity member = findMemberByPrincipal(principal);
     return CharacterTemporaryImageResponse.builder()
-        .characterImageUrl(
-            s3Service.preSignedCharacterImageUrlFrom(retrievedMember.getCharacterImageUrl()))
+        .characterImageUrl(s3Service.preSignedCharacterImageUrlFrom(member.getCharacterImageUrl()))
         .build();
+  }
+
+  public void createCharacterImage(
+      TodakUser principal, MultipartFile file, AiCharacterRequest dto) {
+    findMemberByPrincipal(principal);
+    aiService.callCharacter(file, dto);
+  }
+
+  public CharacterRegisterResponse changeRoleAndReissueToken(
+      TodakUser principal, HttpServletResponse response) {
+    MemberEntity member = findMemberByPrincipal(principal);
+    member.updateRole(TodakRole.ROLE_USER.name());
+
+    var newUser = createNewTodakUser(member);
+    var accessToken = JwtUtils.issueToken(newUser, JwtConstant.ACCESS_TYPE);
+    var refreshToken = JwtUtils.issueToken(newUser, JwtConstant.REFRESH_TYPE);
+
+    updateRefreshTokenCookie(response, refreshToken);
+    return CharacterRegisterResponse.builder().accessToken(accessToken).build();
+  }
+
+  private MemberEntity findMemberByPrincipal(TodakUser principal) {
+    Long id = principal.getId();
+    return memberRepository
+        .findById(id)
+        .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND, id));
+  }
+
+  private TodakUser createNewTodakUser(MemberEntity member) {
+    return TodakUser.builder()
+        .id(member.getId())
+        .username(member.getLoginId())
+        .role(member.getRole().name())
+        .build();
+  }
+
+  private void updateRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+    var refreshCookie = CookieUtils.createValidCookie(REFRESH_TOKEN_COOKIE_KEY, refreshToken);
+    CookieUtils.updateCookie(response, refreshCookie);
   }
 }
