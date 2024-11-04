f6c76318c161d7182b17f5b05aeea2d487d5ec29
Sin Han <vrdhan212@gmail.com>
2024-11-04 17:08:27 +0900

    feat: AI에 캐릭터 생성 요청 TODAK-201

--- src/main/java/com/heartsave/todaktodak_api/ai/dto/request/AiCharacterRequest.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/dto/request/AiCharacterRequest.java
@@ -1,8 +1,6 @@
 package com.heartsave.todaktodak_api.ai.dto.request;
 
 import lombok.Builder;
-import lombok.Getter;
 
 @Builder
-@Getter
 public record AiCharacterRequest(Long memberId, String characterStyle) {}
--- src/main/java/com/heartsave/todaktodak_api/ai/service/AiService.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/service/AiService.java
@@ -1,11 +1,21 @@
 package com.heartsave.todaktodak_api.ai.service;
 
-import com.heartsave.todaktodak_api.ai.dto.AiContentRequest;
-import com.heartsave.todaktodak_api.ai.dto.AiContentResponse;
+import com.heartsave.todaktodak_api.ai.dto.request.AiCharacterRequest;
+import com.heartsave.todaktodak_api.ai.dto.request.AiContentRequest;
+import com.heartsave.todaktodak_api.ai.dto.response.AiContentResponse;
+import com.heartsave.todaktodak_api.ai.exception.AiException;
+import com.heartsave.todaktodak_api.common.exception.errorspec.AiErrorSpec;
 import com.heartsave.todaktodak_api.diary.entity.DiaryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
@@ -13,59 +23,94 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class AiService {
 
-    private final WebClient webClient;
+  private final WebClient webClient;
 
-    public AiContentResponse callAiContent(DiaryEntity diary) {
-        AiContentRequest request = getAiContentRequest(diary);
-        callWebtoon(request);
-        callBgm(request);
-        String comment = callComment(request);
-        return AiContentResponse.builder()
-                .aiComment(comment)
-                .build();
-    }
+  public void callCharacter(MultipartFile image, AiCharacterRequest request) {
+    webClient
+        .post()
+        .uri("/character")
+        .contentType(MediaType.MULTIPART_FORM_DATA)
+        .body(BodyInserters.fromMultipartData(createMultipartBody(image, request)))
+        .retrieve()
+        .bodyToMono(Void.class)
+        .doOnSuccess(result -> log.info("캐릭터 생성 요청을 성공적으로 보냈습니다."))
+        .doOnError(error -> log.error("캐릭터 생성 요청에 오류가 발생했습니다.", error))
+        .subscribe();
+  }
 
-    private AiContentRequest getAiContentRequest(DiaryEntity diary) {
-        return AiContentRequest.builder()
-                .id(diary.getMemberEntity().getId())
-                .content(diary.getContent())
-                .emotion(diary.getEmotion())
-                .build();
-    }
+  private MultiValueMap<String, HttpEntity<?>> createMultipartBody(
+      MultipartFile image, AiCharacterRequest request) {
+    MultiValueMap<String, HttpEntity<?>> multipartBody = new LinkedMultiValueMap<>();
 
-    private void callWebtoon(AiContentRequest request) {
-        webClient
-                .post()
-                .uri("/webtoon")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(Void.class)
-                .doOnSuccess(result -> log.info("Webtoon 생성 요청을 성공적으로 보냈습니다."))
-                .doOnError(error -> log.error("Webtoon 생성 요청에 오류가 발생했습니다.", error))
-                .subscribe();
-    }
+    // 요청 json
+    multipartBody.add("memberId", new HttpEntity<>(request.memberId()));
+    multipartBody.add("characterStyle", new HttpEntity<>(request.characterStyle()));
 
-    private void callBgm(AiContentRequest request) {
-        webClient
-                .post()
-                .uri("/bgm")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(Void.class)
-                .doOnSuccess(result -> log.info("BGM 생성 요청을 성공적으로 보냈습니다."))
-                .doOnError(error -> log.error("BGM 생성 요청에 오류가 발생했습니다.", error))
-                .subscribe();
+    // 이미지
+    try {
+      ByteArrayResource imageResource =
+          new ByteArrayResource(image.getBytes()) {
+            @Override
+            public String getFilename() {
+              return image.getOriginalFilename();
+            }
+          };
+      multipartBody.add("userImage", new HttpEntity<>(imageResource));
+    } catch (Exception e) {
+      throw new AiException(AiErrorSpec.IMAGE_PROCESS_FAIL, image.getOriginalFilename());
     }
+    return multipartBody;
+  }
 
-    private String callComment(AiContentRequest request) {
-        return webClient
-                .post()
-                .uri("/comment")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(String.class)
-                .doOnSuccess(result -> log.info("AI 코멘트 생성 요청을 성공적으로 보냈습니다."))
-                .doOnError(error -> log.error("AI 코멘트 생성 요청에 오류가 발생했습니다."))
-                .block();
-    }
+  public AiContentResponse callAiContent(DiaryEntity diary) {
+    AiContentRequest request = getAiContentRequest(diary);
+    callWebtoon(request);
+    callBgm(request);
+    String comment = callComment(request);
+    return AiContentResponse.builder().aiComment(comment).build();
+  }
+
+  private AiContentRequest getAiContentRequest(DiaryEntity diary) {
+    return AiContentRequest.builder()
+        .id(diary.getMemberEntity().getId())
+        .content(diary.getContent())
+        .emotion(diary.getEmotion())
+        .build();
+  }
+
+  private void callWebtoon(AiContentRequest request) {
+    webClient
+        .post()
+        .uri("/webtoon")
+        .bodyValue(request)
+        .retrieve()
+        .bodyToMono(Void.class)
+        .doOnSuccess(result -> log.info("Webtoon 생성 요청을 성공적으로 보냈습니다."))
+        .doOnError(error -> log.error("Webtoon 생성 요청에 오류가 발생했습니다.", error))
+        .subscribe();
+  }
+
+  private void callBgm(AiContentRequest request) {
+    webClient
+        .post()
+        .uri("/bgm")
+        .bodyValue(request)
+        .retrieve()
+        .bodyToMono(Void.class)
+        .doOnSuccess(result -> log.info("BGM 생성 요청을 성공적으로 보냈습니다."))
+        .doOnError(error -> log.error("BGM 생성 요청에 오류가 발생했습니다.", error))
+        .subscribe();
+  }
+
+  private String callComment(AiContentRequest request) {
+    return webClient
+        .post()
+        .uri("/comment")
+        .bodyValue(request)
+        .retrieve()
+        .bodyToMono(String.class)
+        .doOnSuccess(result -> log.info("AI 코멘트 생성 요청을 성공적으로 보냈습니다."))
+        .doOnError(error -> log.error("AI 코멘트 생성 요청에 오류가 발생했습니다."))
+        .block();
+  }
 }
