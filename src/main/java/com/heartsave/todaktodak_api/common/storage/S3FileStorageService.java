e76b548273cf2e9cf6283a7ef1ee570b886907d8
Sin Han <vrdhan212@gmail.com>
2024-11-04 11:06:03 +0900

    refactor: s3 파일 presign TODAK-193

--- src/main/java/com/heartsave/todaktodak_api/common/storage/S3FileStorageService.java
+++ src/main/java/com/heartsave/todaktodak_api/common/storage/S3FileStorageService.java
@@ -1,36 +1,45 @@
 package com.heartsave.todaktodak_api.common.storage;
 
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @RequiredArgsConstructor
 @Service
-public class S3FileStorageService { // Todo : S3 SDK 연동 및 pre-signed url 부여 로직 작성
-  private final S3Client s3Client;
+public class S3FileStorageService {
+  private final S3Presigner s3Presigner;
 
-  private List<String> getBucketList() {
-    var response = s3Client.listBuckets();
-    return response.buckets().stream().map(Bucket::name).collect(Collectors.toList());
-  }
+  @Value("${aws.s3.bucket-name}")
+  private String BUCKET_NAME;
 
   public List<String> preSignedWebtoonUrlFrom(List<String> s3FolderUrl) {
-    return new ArrayList<>();
+    return s3FolderUrl.stream().map(this::preSign).toList();
   }
 
   public String preSignedFirstWebtoonUrlFrom(String s3FileUrl) {
-    return "";
+    return preSign(s3FileUrl);
   }
 
   public String preSignedCharacterImageUrlFrom(String s3FileUrl) {
-    return "";
+    return preSign(s3FileUrl);
   }
 
   public String preSignedBgmUrlFrom(String s3FileUrl) {
-    return "";
+    return preSign(s3FileUrl);
+  }
+
+  private String preSign(String url) {
+    var presignRequest =
+        GetObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofSeconds(60))
+            .getObjectRequest(GetObjectRequest.builder().bucket(BUCKET_NAME).key(url).build())
+            .build();
+
+    return s3Presigner.presignGetObject(presignRequest).url().toString();
   }
 }
