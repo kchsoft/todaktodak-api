eb6dc2bbec074fd547eb02125fdb988066420dec
kchranger <kchranger@gmail.com>
2024-11-05 17:53:22 +0900

    feat: 일기 작성시 기본 URL 적용 TODAK-211

--- src/main/java/com/heartsave/todaktodak_api/common/storage/S3FileStorageService.java
+++ src/main/java/com/heartsave/todaktodak_api/common/storage/S3FileStorageService.java
@@ -1,9 +1,12 @@
 package com.heartsave.todaktodak_api.common.storage;
 
+import static com.heartsave.todaktodak_api.common.constant.CoreConstant.URL.DEFAULT_URL;
+
 import com.heartsave.todaktodak_api.common.config.properties.S3Properties;
 import java.time.Duration;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
@@ -11,6 +14,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class S3FileStorageService {
   private final S3Presigner s3Presigner;
@@ -22,6 +26,8 @@ public class S3FileStorageService {
   }
 
   public String preSignedFirstWebtoonUrlFrom(String key) {
+    log.info("this is key = {}", key);
+    log.info("this is key = {}", s3Properties.defaultKey().webtoon());
     return key == null ? preSign(s3Properties.defaultKey().webtoon()) : preSign(key);
   }
 
@@ -35,6 +41,8 @@ public class S3FileStorageService {
 
   // TODO: presigned url 캐싱 관리
   private String preSign(String key) throws NoSuchKeyException {
+    if (key.equals(DEFAULT_URL)) return DEFAULT_URL;
+
     var preSignRequest =
         GetObjectPresignRequest.builder()
             .signatureDuration(Duration.ofSeconds(s3Properties.presignDuration()))
