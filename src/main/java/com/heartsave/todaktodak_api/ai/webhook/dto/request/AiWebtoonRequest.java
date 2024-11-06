e4448fad1255c8346be94367d55e5c684cb4198e
kchranger <kchranger@gmail.com>
2024-11-06 17:07:09 +0900

    feat: request 객체 필드에 @NotNull 추가 TODAK-209
    - AiWebtoonRequest의 필드에 @NotNull을 추가

--- src/main/java/com/heartsave/todaktodak_api/ai/webhook/dto/request/AiWebtoonRequest.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/webhook/dto/request/AiWebtoonRequest.java
@@ -2,10 +2,11 @@ package com.heartsave.todaktodak_api.ai.webhook.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 public record AiWebtoonRequest(
-    @Min(1L) Long memberId,
-    @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate createdDate,
+    @NotNull @Min(1L) Long memberId,
+    @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate createdDate,
     @NotBlank String webtoonFolderUrl) {}
