0ce42d0fbd2b8b978e60a899a8433dcc6733b02d
Sin Han <vrdhan212@gmail.com>
2024-11-07 12:37:55 +0900

    refactor: 캐릭터 생성 완료 웹훅 요청 dto 유효성 검사 TODAK-203

--- src/main/java/com/heartsave/todaktodak_api/ai/webhook/dto/request/AiWebhookCharacterRequest.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/webhook/dto/request/AiWebhookCharacterRequest.java
@@ -1,14 +1,21 @@
 package com.heartsave.todaktodak_api.ai.webhook.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
 @Builder
 @Schema(description = "AI 서버의 캐릭터 생성 완료 알림 요청 객체")
 public record AiWebhookCharacterRequest(
-    @Schema(example = "1", description = "회원 아이디") Long memberId,
-    @Schema(example = "{ \"hairs\" : \"long,black\" }", description = "캐릭터 특징")
+    @NotNull @Schema(example = "1", description = "회원 아이디") Long memberId,
+    @NotBlank @Schema(example = "{ \"hairs\" : \"long,black\" }", description = "캐릭터 특징")
         String characterInfo,
-    @Schema(example = "romance", description = "캐릭터 화풍") String characterStyle,
-    @Schema(example = "13564", description = "캐릭터 이미지 시드") Integer seedNum,
-    @Schema(example = "character/memberId", description = "캐릭터 이미지 경로") String characterUrl) {}
+    @NotBlank @Schema(example = "romance", description = "캐릭터 화풍") String characterStyle,
+    @NotNull @Positive @Schema(example = "13564", description = "캐릭터 이미지 시드") Integer seedNum,
+    @NotBlank
+        @Pattern(regexp = "^.+/.+$") // 슬래시 앞뒤에 문자 존재
+        @Schema(example = "character/memberId", description = "캐릭터 이미지 경로")
+        String characterUrl) {}
