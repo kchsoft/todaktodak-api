5aaa1813c3284f81eca3a8386e98fe94ea6a9337
Sin Han <vrdhan212@gmail.com>
2024-11-08 00:07:40 +0900

    test: 캐릭터 웹훅 알림 전송 TODAK-229

--- src/test/java/com/heartsave/todaktodak_api/ai/webhook/service/AiWebhookCharacterServiceTest.java
+++ src/test/java/com/heartsave/todaktodak_api/ai/webhook/service/AiWebhookCharacterServiceTest.java
@@ -5,14 +5,20 @@ import static org.mockito.Mockito.*;
 
 import com.heartsave.todaktodak_api.ai.webhook.dto.request.WebhookCharacterCompletionRequest;
 import com.heartsave.todaktodak_api.common.BaseTestObject;
+import com.heartsave.todaktodak_api.common.exception.errorspec.EventErrorSpec;
+import com.heartsave.todaktodak_api.event.constant.EventType;
+import com.heartsave.todaktodak_api.event.entity.EventEntity;
+import com.heartsave.todaktodak_api.event.exception.EventException;
 import com.heartsave.todaktodak_api.event.service.EventService;
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
 import com.heartsave.todaktodak_api.member.exception.MemberNotFoundException;
 import com.heartsave.todaktodak_api.member.repository.MemberRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -40,21 +46,50 @@ final class AiWebhookCharacterServiceTest {
   }
 
   @Test
-  void saveCharacterAndNotify() {
+  @DisplayName("캐릭터 웹훅에 대한 알림 성공")
+  void saveCharacterAndNotify_success() {
     // given
     when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+    ArgumentCaptor<EventEntity> eventCaptor = ArgumentCaptor.forClass(EventEntity.class);
 
     // when
     characterService.saveCharacterAndNotify(request);
 
     // then
     verify(memberRepository).findById(member.getId());
+    verify(eventService).send(eventCaptor.capture());
+
+    // 회원 정보 변경
     assertEquals(request.characterInfo(), member.getCharacterInfo());
     assertEquals(request.characterStyle(), member.getCharacterStyle());
     assertEquals(request.seedNum(), member.getCharacterSeed());
+
+    // 알림 메시지
+    EventEntity capturedEvent = eventCaptor.getValue();
+    assertAll(
+        () -> assertEquals(member, capturedEvent.getMemberEntity()),
+        () -> assertEquals(EventType.CHARACTER.getType(), capturedEvent.getEventName()));
   }
 
   @Test
+  @DisplayName("캐릭터 웹훅에 대한 알림 실패 - 이벤트 전송 오류")
+  void saveCharacterAndNotify_eventSendFail() {
+    // given
+    when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+    doThrow(new EventException(EventErrorSpec.EVENT_CONNECT_FAIL))
+        .when(eventService)
+        .send(any(EventEntity.class));
+
+    // when
+    assertThrows(EventException.class, () -> characterService.saveCharacterAndNotify(request));
+
+    // then
+    verify(memberRepository).findById(member.getId());
+    verify(eventService).send(any(EventEntity.class));
+  }
+
+  @Test
+  @DisplayName("캐릭터 웹훅에 대한 알림 실패 - 존재하지 않는 회원")
   void saveCharacterAndNotify_memberNotFound() {
     // given
     when(memberRepository.findById(member.getId())).thenReturn(Optional.empty());
@@ -64,5 +99,6 @@ final class AiWebhookCharacterServiceTest {
         MemberNotFoundException.class, () -> characterService.saveCharacterAndNotify(request));
 
     verify(memberRepository).findById(member.getId());
+    verify(eventService, never()).send(any(EventEntity.class));
   }
 }
