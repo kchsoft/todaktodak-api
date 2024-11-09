50e2faf97f7f73c6a84d179b86bc76df341ea1d8
Sin Han <vrdhan212@gmail.com>
2024-11-09 09:24:24 +0900

    fix: 이미지 multipart 구성 오류 TODAK-229

--- src/main/java/com/heartsave/todaktodak_api/ai/client/service/AiClientService.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/client/service/AiClientService.java
@@ -9,12 +9,15 @@ import com.heartsave.todaktodak_api.ai.exception.AiException;
 import com.heartsave.todaktodak_api.common.exception.errorspec.AiErrorSpec;
 import com.heartsave.todaktodak_api.diary.entity.DiaryEntity;
 import com.heartsave.todaktodak_api.member.entity.MemberEntity;
+import java.io.IOException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -94,9 +97,9 @@ public class AiClientService {
         .subscribe();
   }
 
-  private MultiValueMap<String, HttpEntity<?>> createMultipartBody(
+  private MultiValueMap<String, Object> createMultipartBodys(
       MultipartFile image, ClientCharacterRequest request) {
-    MultiValueMap<String, HttpEntity<?>> multipartBody = new LinkedMultiValueMap<>();
+    MultiValueMap<String, Object> multipartBody = new LinkedMultiValueMap<>();
 
     // 요청 json
     multipartBody.add("memberId", new HttpEntity<>(request.memberId()));
@@ -116,5 +119,35 @@ public class AiClientService {
       throw new AiException(AiErrorSpec.IMAGE_PROCESS_FAIL, image.getOriginalFilename());
     }
     return multipartBody;
+  }
+
+  private MultiValueMap<String, HttpEntity<?>> createMultipartBody(
+      MultipartFile image, ClientCharacterRequest request) {
+    MultipartBodyBuilder builder = new MultipartBodyBuilder();
+
+    // 요청 json
+    builder.part("memberId", request.memberId());
+    builder.part("characterStyle", request.characterStyle());
+
+    // 이미지
+    try {
+      ByteArrayResource imageResource =
+          new ByteArrayResource(image.getBytes()) {
+            @Override
+            public String getFilename() {
+              return image.getOriginalFilename();
+            }
+          };
+
+      builder
+          .part("userImage", imageResource)
+          .filename(Objects.requireNonNull(image.getOriginalFilename()))
+          .contentType(MediaType.parseMediaType(Objects.requireNonNull(image.getContentType())));
+
+    } catch (IOException e) {
+      throw new AiException(AiErrorSpec.IMAGE_PROCESS_FAIL, image.getOriginalFilename());
+    }
+
+    return builder.build();
   }
 }
