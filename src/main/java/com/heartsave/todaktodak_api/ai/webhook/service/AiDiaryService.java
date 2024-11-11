e5ccd590681618973cf805679f35aada17a76893
kchranger <kchranger@gmail.com>
2024-11-11 09:12:17 +0900

    feat: 웹툰/BGM 생성 완료 알림 적용 TODAK-229

--- src/main/java/com/heartsave/todaktodak_api/ai/webhook/service/AiDiaryService.java
+++ src/main/java/com/heartsave/todaktodak_api/ai/webhook/service/AiDiaryService.java
@@ -3,7 +3,13 @@ package com.heartsave.todaktodak_api.ai.webhook.service;
 import com.heartsave.todaktodak_api.ai.webhook.dto.request.WebhookBgmCompletionRequest;
 import com.heartsave.todaktodak_api.ai.webhook.dto.request.WebhookWebtoonCompletionRequest;
 import com.heartsave.todaktodak_api.ai.webhook.repository.AiRepository;
+import com.heartsave.todaktodak_api.common.exception.errorspec.MemberErrorSpec;
 import com.heartsave.todaktodak_api.common.storage.s3.S3FileStorageService;
+import com.heartsave.todaktodak_api.event.constant.EventType;
+import com.heartsave.todaktodak_api.event.entity.EventEntity;
+import com.heartsave.todaktodak_api.event.service.EventService;
+import com.heartsave.todaktodak_api.member.exception.MemberNotFoundException;
+import com.heartsave.todaktodak_api.member.repository.MemberRepository;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +24,8 @@ public class AiDiaryService {
 
   private final AiRepository aiRepository;
   private final S3FileStorageService s3FileStorageService;
-
-  //  private final EventService eventService;
-  //  private final MemberRepository memberRepository;
+  private final EventService eventService;
+  private final MemberRepository memberRepository;
 
   public void saveWebtoon(WebhookWebtoonCompletionRequest request) {
     Long memberId = request.memberId();
@@ -34,8 +39,7 @@ public class AiDiaryService {
     }
     log.info("webtoon url 업데이트를 마쳤습니다.");
     if (aiRepository.isContentCompleted(memberId, createdDate)) { // Todo : Lock 설정을 통한 동시성 통제
-      // Todo : SSE 알림 구현
-      //      logEventAndNotify(memberId);
+      notify(memberId);
     }
   }
 
@@ -51,22 +55,20 @@ public class AiDiaryService {
     }
     log.info("Bgm url 업데이트를 마쳤습니다. memberId={}", memberId);
     if (aiRepository.isContentCompleted(memberId, createdDate)) { // Todo : Lock 설정을 통한 동시성 통제
-      // Todo : SSE 알림 구현
-      //      logEventAndNotify(memberId);
+      notify(memberId);
     }
   }
 
-  //  private void logEventAndNotify(Long memberId) {
-  //    var member =
-  //        memberRepository
-  //            .findById(memberId)
-  //            .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND,
-  // memberId));
-  //    eventService.saveAndSend(
-  //        EventEntity.builder()
-  //            .memberEntity(member)
-  //            .eventName(EventType.DIARY.getType())
-  //            .eventData("일기가 생성됐습니다.")
-  //            .build());
-  //  }
+  private void notify(Long memberId) {
+    var member =
+        memberRepository
+            .findById(memberId)
+            .orElseThrow(() -> new MemberNotFoundException(MemberErrorSpec.NOT_FOUND, memberId));
+    eventService.send(
+        EventEntity.builder()
+            .memberEntity(member)
+            .eventName(EventType.DIARY.getType())
+            .eventData("일기가 생성됐습니다.")
+            .build());
+  }
 }
