f91f419393e79a01fc28d702a01050d3a5ce9621
kchranger <kchranger@gmail.com>
2024-11-19 15:13:54 +0900

    remove: AI API KEY 검증 삭제 TODAK-249
    - Interceptor 설정 삭제
    - yaml에서 환경 변수 삭제
    - API KEY 관련 예외 삭제

--- src/main/java/com/heartsave/todaktodak_api/common/config/WebConfig.java
+++ src/main/java/com/heartsave/todaktodak_api/common/config/WebConfig.java
@@ -1,24 +1,15 @@
 package com.heartsave.todaktodak_api.common.config;
 
-import com.heartsave.todaktodak_api.ai.webhook.interceptor.AiServerApiKeyInterceptor;
 import com.heartsave.todaktodak_api.auth.annotation.TodakUserIdResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-  private final AiServerApiKeyInterceptor aiServerApiKeyInterceptor;
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(aiServerApiKeyInterceptor).addPathPatterns("/api/v1/webhook/ai/**");
-  }
 
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
