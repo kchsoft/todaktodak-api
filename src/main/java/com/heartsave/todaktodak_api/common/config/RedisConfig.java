4a62d1c214b3616b7482fc35149037b902c50a94
Sin Han <vrdhan212@gmail.com>
2024-11-19 15:33:05 +0900

    remove: 불필요한 redis template TODAK-252

--- src/main/java/com/heartsave/todaktodak_api/common/config/RedisConfig.java
+++ src/main/java/com/heartsave/todaktodak_api/common/config/RedisConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -23,15 +21,5 @@ public class RedisConfig {
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
     return new LettuceConnectionFactory(REDIS_HOST, REDIS_PORT);
-  }
-
-  @Bean
-  public RedisTemplate<String, String> otpRedisTemplate(
-      RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(redisConnectionFactory);
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new StringRedisSerializer());
-    return redisTemplate;
   }
 }
