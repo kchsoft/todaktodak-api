033782bbe2eba6a7d8e4bfb35dbc3e78787ea628
kchranger <kchranger@gmail.com>
2024-11-19 21:21:14 +0900

    refactor: 일기 작성 글자 제한 변경 TODAK-249

--- src/main/java/com/heartsave/todaktodak_api/diary/constant/DiaryContentConstraintConstant.java
+++ src/main/java/com/heartsave/todaktodak_api/diary/constant/DiaryContentConstraintConstant.java
@@ -1,7 +1,7 @@
 package com.heartsave.todaktodak_api.diary.constant;
 
 public class DiaryContentConstraintConstant {
-  public static final int DIARY_CONTENT_MAX_SIZE = 5000;
-  public static final int DIARY_CONTENT_MIN_SIZE = 300;
-  public static final int PUBLIC_DIARY_CONTENT_MAX_SIZE = 5000;
+  public static final int DIARY_CONTENT_MAX_SIZE = 3000;
+  public static final int DIARY_CONTENT_MIN_SIZE = 100;
+  public static final int PUBLIC_DIARY_CONTENT_MAX_SIZE = 3000;
 }
