16adffb6ea66d0c7ada7fc18a615d4c792f0a7dc
Sin Han <vrdhan212@gmail.com>
2024-11-22 15:31:32 +0900

    feat: otp 비밀번호 캐시 레포지토리 TODAK-252

--- src/main/java/com/heartsave/todaktodak_api/auth/repository/RefreshTokenCacheRepository.java
+++ src/main/java/com/heartsave/todaktodak_api/auth/repository/RefreshTokenCacheRepository.java
@@ -1,24 +1,31 @@
 package com.heartsave.todaktodak_api.auth.repository;
 
+import static com.heartsave.todaktodak_api.common.config.constant.RedisConstant.REFRESH_TOKEN_KEY_PREFIX;
+
+import com.heartsave.todaktodak_api.common.security.config.properties.JwtProperties;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class RefreshTokenCacheRepository {
-  @Value("${jwt.refresh-expire-time}")
-  private Long TTL;
+  private final JwtProperties jwtProperties;
 
-  private static final String KEY_PREFIX = "REFRESH_TOKEN:";
   private final RedisTemplate<String, String> redisTemplate;
 
   public void set(String key, String value) {
-    redisTemplate.opsForValue().set(KEY_PREFIX + key, value, TTL);
+    redisTemplate
+        .opsForValue()
+        .set(
+            REFRESH_TOKEN_KEY_PREFIX + key,
+            value,
+            jwtProperties.refreshExpireTime() / 1000,
+            TimeUnit.SECONDS);
   }
 
   public String get(String key) {
-    return redisTemplate.opsForValue().get(KEY_PREFIX + key);
+    return redisTemplate.opsForValue().get(REFRESH_TOKEN_KEY_PREFIX + key);
   }
 }
