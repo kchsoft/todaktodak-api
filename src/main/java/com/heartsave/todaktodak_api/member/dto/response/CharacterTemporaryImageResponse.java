3f92a4ee72e7662df5f322c78eb338b71b88de13
Sin Han <vrdhan212@gmail.com>
2024-11-25 16:53:04 +0900

    refactor: 캐릭터 이미지 응답 dto 필드 추가 TODAK-257
    캐싱된 캐릭터 이미지 경로

--- src/main/java/com/heartsave/todaktodak_api/member/dto/response/CharacterTemporaryImageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.heartsave.todaktodak_api.member.dto.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-
-@Builder
-@Schema(description = "회원 캐릭터 생성 과정에서 기존에 생성된 회원 캐릭터 이미지 응답 객체")
-public record CharacterTemporaryImageResponse(
-    @Schema(example = "character/123", description = "회원 캐릭터 이미지 주소") String characterImageUrl) {}
