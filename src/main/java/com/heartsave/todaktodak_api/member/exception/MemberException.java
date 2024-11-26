af6cef40b5cdf0b6d89e5d4df94593174a65dfcd
Sin Han <vrdhan212@gmail.com>
2024-11-26 17:38:11 +0900

    feat: 캐릭터 등록 관련 에러 명세 TODAK-257

--- src/main/java/com/heartsave/todaktodak_api/member/exception/MemberException.java
+++ src/main/java/com/heartsave/todaktodak_api/member/exception/MemberException.java
@@ -1,13 +1,12 @@
 package com.heartsave.todaktodak_api.member.exception;
 
-
 import com.heartsave.todaktodak_api.common.exception.BaseException;
 import com.heartsave.todaktodak_api.common.exception.ErrorFieldBuilder;
 import com.heartsave.todaktodak_api.common.exception.errorspec.MemberErrorSpec;
 
 public class MemberException extends BaseException {
 
-  protected MemberException(MemberErrorSpec errorSpec, Long memberId) {
+  public MemberException(MemberErrorSpec errorSpec, Long memberId) {
     super(errorSpec, ErrorFieldBuilder.builder().add("memberId", memberId).build());
   }
 }
