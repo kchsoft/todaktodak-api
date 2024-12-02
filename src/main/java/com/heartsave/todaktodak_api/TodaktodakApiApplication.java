1f635aadc47a65f4546014598daaf1d8548a4528
kchranger <kchranger@gmail.com>
2024-12-03 00:49:30 +0900

    feat: app 및 test 시간을 utc로 고정 TODAK-260

--- src/main/java/com/heartsave/todaktodak_api/TodaktodakApiApplication.java
+++ src/main/java/com/heartsave/todaktodak_api/TodaktodakApiApplication.java
@@ -1,5 +1,6 @@
 package com.heartsave.todaktodak_api;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class TodaktodakApiApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     SpringApplication.run(TodaktodakApiApplication.class, args);
   }
 }
