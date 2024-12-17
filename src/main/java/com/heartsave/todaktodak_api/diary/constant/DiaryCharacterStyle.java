45602a9cc8fcd6374838f5d02af77d203aebc1a0
Sin Han <vrdhan212@gmail.com>
2024-12-17 14:16:55 +0900

    feat: 웹툰 화풍 enum TODAK-299

---
+++ src/main/java/com/heartsave/todaktodak_api/diary/constant/DiaryCharacterStyle.java
@@ -0,0 +1,12 @@
+package com.heartsave.todaktodak_api.diary.constant;
+
+public enum DiaryCharacterStyle {
+  ROMANCE("romance"),
+  PIXAR("pixar");
+
+  private final String style;
+
+  DiaryCharacterStyle(String style) {
+    this.style = style;
+  }
+}
